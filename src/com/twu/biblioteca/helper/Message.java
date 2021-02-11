4da35de169519f8a434900df3597200dc8a75cb7
joaovictor.marques <joaovictor.marques@joaovictormarques.local>
2021-02-11 19:28:25 -0300

    feat(Message): add class to manage messages

---
+++ src/com/twu/biblioteca/helper/Message.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca.helper;
+
+public class Message {
+    public static final String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    public static final String QUIT = "";
+    public static final String CHECKOUT_SUCCESSFUL = "Thank you! Enjoy the book.";
+    public static final String CHECKOUT_UNSUCCESSFUL = "Sorry, that book is not available.";
+    public static final String RETURN_SUCCESSFUL = "Thank you for returning the book.";
+    public static final String RETURN_UNSUCCESSFUL = "That is not a valid book to return.";
+    public static final String BOOK_AVAILABLE_LIST_IS_EMPTY = "There is no book to checkout.";
+    public static final String BOOK_NOT_AVAILABLE_LIST_IS_EMPTY = "There is no book to return.";
+}
